a074725fd1ce5f0b84350558969b8cd719df8eb9
ISAAC TAN HAO <tyhisaac2@graduate.utm.my>
2021-05-21 15:13:24 +0800

    added some grid box and change apk icon

--- lib/screen/main_screen/body.dart
+++ lib/screen/main_screen/body.dart
@@ -13,6 +13,31 @@ class _bodystate extends State<Body> {
   Widget build(BuildContext context) {
     return Scaffold(
       //child: child,
+      body: GridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        padding: const EdgeInsets.all(20),
+        children: <Widget>[
+          //set boxes
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: const Text("data"),
+            color: Colors.amber,
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: const Text("data1"),
+            color: Colors.amber,
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: const Text("data2"),
+            color: Colors.amber,
+          )
+        ],
+      ),
+
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 1,
         items: [
