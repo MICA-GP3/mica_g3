d260b24a7d7e94d758168a23362f6811415485c3
Nur Atiqah Mohd Fua'ad <47960949+atqhfuaad@users.noreply.github.com>
2021-05-28 11:15:21 +0800

    adding mvvm structure - Atiqah

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,6 @@
 //GROUP3 - MICA [MIDAH - ISAAC - CHU NING - ATIQAH]
 //HASTA CAR RENTAL
 
-//PULL By IZ
-
-//hello from cn
-
-//PULL By Atiqah
-
-//hello from atqhcomel
-
-//PULL test from hamidah
-
 import 'package:flutter/material.dart';
 import 'package:hasta_rental/screen/main_screen/mainpage.dart';
 
