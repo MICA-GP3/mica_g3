7809340131917587b56d95802288d9d9aa5d324c
Nur Atiqah Mohd Fua'ad <47960949+atqhfuaad@users.noreply.github.com>
2021-06-15 22:15:22 +0800

    Build UI for Car List, Booking and Booking History Screen

---
+++ lib/screens/car_list_screen/car_list.dart
@@ -0,0 +1,80 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:hasta_rental/widgets/appbar.dart';
+
+class CarListPage extends StatefulWidget {
+  @override
+  _CarListPage createState() => _CarListPage();
+}
+
+class _CarListPage extends State<CarListPage> {
+  int _index = 0;
+
+  Future<FirebaseApp> _initialize() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: Bar(),
+        endDrawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              DrawerHeader(child: Text('Header')),
+              ListTile(
+                title: Text('Profile'),
+                onTap: () => {},
+              ),
+              Expanded(
+                  child: Align(
+                alignment: Alignment.bottomCenter,
+                child: ListTile(
+                  leading: Icon(Icons.logout),
+                  title: Text("Log Out"),
+                  tileColor: Colors.amber,
+                  onTap: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                              //title: Text("Log Out?"),
+                              content: Text("Are you sure?"),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {}, child: Text("Yes")),
+                                TextButton(onPressed: () {}, child: Text("No")),
+                              ],
+                            ));
+                  },
+                ),
+              )),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _index,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: 'Calendar',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: 'Notification',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
