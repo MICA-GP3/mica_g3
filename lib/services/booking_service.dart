4cc0f3dd16056b0f1191873ce82b5ca11956c603
Isaac Tan <isaac@graduate.utm.my>
2021-07-03 16:37:20 +0800

    add booking service

---
+++ lib/services/booking_service.dart
@@ -0,0 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+final CollectionReference _mainCollection = _firestore.collection('booking');
+
+class Booking {}
